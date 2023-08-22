ae8c65c97406318485ebf29c9fd6e2b86a0bd97d
Nahuel Morales <nahuelmorales154@gmail.com>
2023-08-22 20:42:26 -0300

    cambios generales

--- Pruba_Sistema_Flask/Pruebas_Flask.py
+++ Pruba_Sistema_Flask/Pruebas_Flask.py
@@ -7,10 +7,10 @@
 #                                para no estar reiniciando el servidor en cada cambio.
 #                                -> Esto siempre que este el if (__name__ == '__main__'): al final del codigo
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)           #Para saber si esta siendo ejecutando desde un archivo principal o esta siendo importada.
+app = Flask(__name__)                       #Para saber si esta siendo ejecutando desde un archivo principal o esta siendo importada.
 app.config['MYSQL_HOST'] = "localhost"          #Host
 app.config['MYSQL_USER'] = "root"               #Usuario de phpmyadmin
 app.config['MYSQL_PASSWORD'] = ""               #Contraseña de phpmyadmin
@@ -18,15 +18,39 @@ app.config['MYSQL_DB'] = "pintureria_pehuajo"   #Base de datos (No tabla)
 mysql = MySQL(app)
 
 #Rutas#
-@app.route("/")                 #Decorador, ruta principal.
+@app.route("/")                             #Decorador, ruta principal.
 def Index():
     return render_template("index.html")
 
-@app.route("/prueba")
-def Prueba():
-    return render_template("prueba.html")
+@app.route("/carga-de-Productos")
+def CargaProducto():
+    return render_template("carga-productos.html")
 
-    #ABM#
+@app.route("/venta")
+def Venta():
+    return render_template("venta.html")
+
+@app.route("/Empresa")
+def Empresa():
+    return render_template("empresa.html")
+
+@app.route("/productos")
+def Productos():
+    return render_template("productos.html")
+
+@app.route("/clientes")
+def Clientes():
+    return render_template("clientes.html")
+
+@app.route("/proveedores")
+def Proveedores():
+    return render_template("proveedores.html")
+
+@app.route("/reportes")
+def Reportes():
+    return render_template("reportes.html")
+
+#-------------------- ABM --------------------#
 @app.route("/add_product", methods=['POST'])
 def add_product():
     if request.method == 'POST':
@@ -40,8 +64,7 @@ def add_product():
         mysql.connection.commit()
         
         print(f"nombre: {nombre}\nTipo: {tipo}\nCantidad: {cantidad},\nPrecio: ${precio}")
-    return 'recived'
-
+    return redirect(url_for('Productos'))
 
 @app.route("/edit_product")
 def edit_product():
@@ -51,6 +74,6 @@ def edit_product():
 def delete_product():
     return "delete_product"
 
-#MD#
-if (__name__ == "__main__"):    #Modo desarrollo. (Usar solo en desarrollo, desabilitar en produccion)
+#Modo desarrollo. (Usar solo en desarrollo, desabilitar en produccion)
+if (__name__ == "__main__"):
     app.run(port = 3000, debug=True)
