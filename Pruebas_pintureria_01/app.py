18242d0d61fc1cc406d33cc897fa9309ad9960c9
Nahuel Morales <nahuelmorales154@gmail.com>
2023-11-05 19:57:17 -0300

    cambios generales

--- Pruebas_pintureria_01/app.py
+++ Pruebas_pintureria_01/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash, session
 from flaskext.mysql import MySQL
+import json
+import heapq
 
 # Crear la aplicacion Flask
 app = Flask(__name__, static_url_path='/static')
@@ -489,44 +491,60 @@ def restar_stock(producto_id, cantidad_comprada):
 
     conn.commit()
 
+# Función para aumentar el stock de un producto
+def aumentar_stock(producto_id, cantidad_comprada):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT cantidad FROM productos WHERE id=%s", (producto_id,))
+    stock_actual = cursor.fetchone()[0]
+
+    nuevo_stock = stock_actual + cantidad_comprada
+
+    cursor.execute("UPDATE productos SET cantidad=%s WHERE id=%s", (nuevo_stock, producto_id))
+    conn.commit()
+
+    conn.close()
+
 #procesar compra
 @app.route("/procesar_compra", methods=["POST"])
 def procesar_compra():
     current_page = 'pedido'
     # Obtener la lista de productos y cantidades del pedido
-    lista_productos = ", ".join([f"{item['nombre']} (x{item['cantidad']})" for item in pedido])
+    # lista_productos = ", ".join([f"{item['nombre']} (x{item['cantidad']})" for item in pedido])
+    lista_productos = json.dumps(pedido)
 
     # Calcular el total de la compra
     total_compra = sum(item['precio'] * item['cantidad'] for item in pedido)
 
-    # Obtener el nombre del cliente seleccionado
-    id_cliente = request.form.get('nombre_cliente')
+    # Obtener el nombre del proveedor seleccionado
+    id_proveedor = request.form.get('id_proveedor')
     
-    # Verificar si id_cliente es None y manejarlo
-    if id_cliente is None:
+    # Verificar si id_proveedor es None y manejarlo
+    if id_proveedor is None:
         flash("Error: Selecciona un cliente.")
         return redirect("/pedido")
 
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    # Obtener el nombre del cliente a partir de su ID
-    cursor.execute("SELECT nombre FROM clientes_proveedores WHERE id=%s", (id_cliente,))
-    resultado_cliente = cursor.fetchone()
+    # Obtener el nombre del proveedor a partir de su ID
+    # cursor.execute("SELECT nombre FROM clientes_proveedores WHERE id=%s", (id_proveedor,))
+    # resultado_cliente = cursor.fetchone()
 
     # Verificar si result_cliente es None y manejarlo
-    if resultado_cliente is None:
-        flash("Error: Cliente no encontrado.")
-        return redirect("/pedido")
+    # if resultado_cliente is None:
+    #     flash("Error: Cliente no encontrado.")
+    #     return redirect("/pedido")
 
-    nombre_cliente = resultado_cliente[0]
+    # nombre_cliente = resultado_cliente[0]
 
-    # Obtener el monto pagado por el cliente desde el formulario
-    monto_pagado = request.form.get('pago_cliente')
+    # Obtener el monto pagado por el usuario desde el formulario
+    monto_pagado = request.form.get('pago_a_proveedor')
 
     # Verificar si monto_pagado es None y manejarlo
     if monto_pagado is None:
-        flash("Error: Ingresa el monto pagado por el cliente.")
+        flash("Error: Ingresa el pago.")
         return redirect("/pedido")
 
     # Convertir monto_pagado a float
@@ -539,42 +557,45 @@ def procesar_compra():
     # Calcular la diferencia entre el total y el pago del cliente
     diferencia = monto_pagado - total_compra
 
-    # Obtener el saldo actual del cliente
-    cursor.execute("SELECT saldo FROM clientes_proveedores WHERE id=%s", (id_cliente,))
+    # Obtener el saldo actual del proveedor
+    cursor.execute("SELECT saldo FROM clientes_proveedores WHERE id=%s", (id_proveedor,))
     saldo_actual = cursor.fetchone()[0]
 
     # Calcular el nuevo saldo sumando la diferencia
     nuevo_saldo = saldo_actual + diferencia
 
-    # Actualizar el saldo del cliente en la base de datos
-    cursor.execute("UPDATE clientes_proveedores SET saldo=%s WHERE id=%s", (nuevo_saldo, id_cliente))
+    # Tipo de movimiento 1 (compra)
+    tipo = 1
+
+    # Actualizar el saldo del proveedor en la base de datos
+    cursor.execute("UPDATE clientes_proveedores SET saldo=%s WHERE id=%s", (nuevo_saldo, id_proveedor))
     conn.commit()
 
     # Insertar los datos en la tabla "detalles_compra"
-    sql = "INSERT INTO detalles_compra (productos, total, pago, diferencia_cliente, nombre_cliente) VALUES (%s, %s, %s, %s, %s);"
-    datos = (lista_productos, total_compra, monto_pagado, diferencia, nombre_cliente)
+    sql = "INSERT INTO detalles_compra (productos, total, pago, diferencia_cliente, id_cliente, tipo) VALUES (%s, %s, %s, %s, %s, %s);"
+    datos = (lista_productos, total_compra, monto_pagado, diferencia, id_proveedor, tipo)
     cursor.execute(sql, datos)
     conn.commit()
 
-    # Restar el stock de los productos comprados
+    # Aumentar el stock de los productos comprados
     for item in pedido:
-        restar_stock(item['id'], item['cantidad'])
+        aumentar_stock(item['id'], item['cantidad'])
 
-      # Actualizar la salida de los productos en la base de datos
-    for item in pedido:
-        producto_id = item['id']
-        cantidad_vendida = item['cantidad']
+    # Actualizar la salida de los productos en la base de datos
+    # for item in pedido:
+    #     producto_id = item['id']
+    #     cantidad_vendida = item['cantidad']
 
         # Obtener la salida actual del producto
-        cursor.execute("SELECT salida FROM productos WHERE id=%s", (producto_id,))
-        salida_actual = cursor.fetchone()[0]
+        # cursor.execute("SELECT salida FROM productos WHERE id=%s", (producto_id,))
+        # salida_actual = cursor.fetchone()[0]
 
-        # Calcular la nueva salida sumando la cantidad vendida
-        nueva_salida = salida_actual + cantidad_vendida
+        # Calcular la nueva salida sumando la cantidad vendida // MAMAAAA
+        # nueva_salida = salida_actual + cantidad_vendida
 
-        # Actualizar la salida del producto en la base de datos
-        cursor.execute("UPDATE productos SET salida=%s WHERE id=%s", (nueva_salida, producto_id))
-        conn.commit()
+        # Actualizar la salida del producto en la base de datos // MAMAAAA
+        # cursor.execute("UPDATE productos SET salida=%s WHERE id=%s", (nueva_salida, producto_id))
+        # conn.commit()
 
     # Limpiar el pedido despues de procesar la compra
     pedido.clear()
@@ -603,13 +624,14 @@ def mostrar_orden_venta():
 def procesar_venta():
     current_page = 'orden_venta'
     # Obtener la lista de productos y cantidades de la venta
-    lista_productos = ", ".join([f"{item['nombre']} (x{item['cantidad']})" for item in lista_vta])
+    # lista_productos = ", ".join([f"{item['nombre']} (x{item['cantidad']})" for item in lista_vta])
+    lista_productos = json.dumps(lista_vta)
 
     # Calcular el total de la venta
-    total_compra = sum(item['precio'] * item['cantidad'] for item in lista_vta)
+    total_venta = sum(item['precio'] * item['cantidad'] for item in lista_vta)
 
     # Obtener el nombre del cliente seleccionado
-    id_cliente = request.form.get('nombre_cliente')
+    id_cliente = request.form.get('id_cliente')
     
     # Verificar si id_cliente es None y manejarlo
     if id_cliente is None:
@@ -620,15 +642,15 @@ def procesar_venta():
     cursor = conn.cursor()
 
     # Obtener el nombre del cliente a partir de su ID
-    cursor.execute("SELECT nombre FROM clientes_proveedores WHERE id=%s", (id_cliente,))
-    resultado_cliente = cursor.fetchone()
+    # cursor.execute("SELECT nombre FROM clientes_proveedores WHERE id=%s", (id_cliente,))
+    # resultado_cliente = cursor.fetchone()
 
     # Verificar si result_cliente es None y manejarlo
-    if resultado_cliente is None:
-        flash("Error: Cliente no encontrado.")
-        return redirect("/orden_venta")
+    # if resultado_cliente is None:
+    #     flash("Error: Cliente no encontrado.")
+    #     return redirect("/orden_venta")
 
-    nombre_cliente = resultado_cliente[0]
+    # nombre_cliente = resultado_cliente[0]
 
     # Obtener el monto pagado por el cliente desde el formulario
     monto_pagado = request.form.get('pago_cliente')
@@ -646,7 +668,7 @@ def procesar_venta():
         return redirect("/orden_venta")
 
     # Calcular la diferencia entre el total y el pago del cliente
-    diferencia = monto_pagado - total_compra
+    diferencia = monto_pagado - total_venta
 
     # Obtener el saldo actual del cliente
     cursor.execute("SELECT saldo FROM clientes_proveedores WHERE id=%s", (id_cliente,))
@@ -655,13 +677,16 @@ def procesar_venta():
     # Calcular el nuevo saldo sumando la diferencia
     nuevo_saldo = saldo_actual + diferencia
 
+    # Tipo de movimiento 2 (venta)
+    tipo = 2
+
     # Actualizar el saldo del cliente en la base de datos
     cursor.execute("UPDATE clientes_proveedores SET saldo=%s WHERE id=%s", (nuevo_saldo, id_cliente))
     conn.commit()
 
     # Insertar los datos en la tabla "detalles_compra"
-    sql = "INSERT INTO detalles_compra (productos, total, pago, diferencia_cliente, nombre_cliente) VALUES (%s, %s, %s, %s, %s);"
-    datos = (lista_productos, total_compra, monto_pagado, diferencia, nombre_cliente)
+    sql = "INSERT INTO detalles_compra (productos, total, pago, diferencia_cliente, id_cliente, tipo) VALUES (%s, %s, %s, %s, %s, %s);"
+    datos = (lista_productos, total_venta, monto_pagado, diferencia, id_cliente, tipo)
     cursor.execute(sql, datos)
     conn.commit()
 
@@ -669,7 +694,7 @@ def procesar_venta():
     for item in lista_vta:
         restar_stock(item['id'], item['cantidad'])
 
-      # Actualizar la salida de los productos en la base de datos
+    # Actualizar la salida de los productos en la base de datos
     for item in lista_vta:
         producto_id = item['id']
         cantidad_vendida = item['cantidad']
@@ -710,13 +735,22 @@ def ranking():
 
     productos_ranking = cursor.fetchall()
 
+    lista_temp = []
+    suma = 0
+    for i in productos_ranking:
+        lista_temp.append([i[8], i[1]])
+        suma = suma + i[8]
+
+    mas_vendido = heapq.nlargest(5, lista_temp)
+    mas_vendido_total = suma
+
     conn.commit()
 
     # Consulta adicional para obtener los tipos disponibles
     cursor.execute("SELECT DISTINCT tipo FROM productos;")
     tipos = [tipo[0] for tipo in cursor.fetchall()]
 
-    return render_template("productos/ranking.html", current_page=current_page, productos_ranking=productos_ranking, tipos=tipos, tipo_filtro=tipo_filtro)
+    return render_template("productos/ranking.html", current_page=current_page, productos_ranking=productos_ranking, tipos=tipos, tipo_filtro=tipo_filtro, mas_vendido=mas_vendido, mas_vendido_total=mas_vendido_total)
 
 # Ruta para resetear el ranking
 @app.route('/reset_ranking', methods=['POST'])
@@ -854,46 +888,176 @@ def realizar_pago(id):
 
 @app.route('/procesar_pago/<int:id>', methods=['POST'])
 def procesar_pago(id):
-    if request.method == 'POST':
-        try:
-            # Conseguir monto del formulario
-            monto_pagado = float(request.form.get('monto'))
+    # if request.method == 'POST':
+    #     try:
+    # Conseguir monto del formulario
+    monto_pagado = float(request.form.get('monto'))
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
 
-            # conseguir info del cliente antes de realizar el pago
-            cursor.execute("SELECT nombre, saldo FROM clientes_proveedores WHERE id=%s", (id,))
-            cliente_info = cursor.fetchone()
+    # conseguir info del cliente antes de realizar el pago
+    cursor.execute("SELECT id, saldo, tipo FROM clientes_proveedores WHERE id=%s", (id,))
+    cliente_info = cursor.fetchone()
 
-            nombre_cliente = cliente_info[0]
-            saldo_actual = cliente_info[1]
+    id_cliente = cliente_info[0]
+    saldo_actual = cliente_info[1]
+    tipo_cliente = cliente_info[2]
 
-            # Calcula la nueva deuda sumando el monto pagado al saldo actual
-            nueva_deuda = saldo_actual + monto_pagado
+    # Calcula la nueva deuda sumando el monto pagado al saldo actual
+    nueva_deuda = saldo_actual + monto_pagado
 
-            # Inserta la información en la tabla detalles_compras
-            cursor.execute("INSERT INTO detalles_compra (productos, total, pago, diferencia_cliente, nombre_cliente) VALUES (%s, %s, %s, %s, %s)",
-                           ('Pago de saldo', saldo_actual, monto_pagado, nueva_deuda, nombre_cliente))
-            conn.commit()
+    # Tipo de movimiento 3 (pago deuda)
+    tipo = 3
+    
+    sql = "INSERT INTO detalles_compra (productos, total, pago, diferencia_cliente, id_cliente, tipo) VALUES (%s, %s, %s, %s, %s, %s);"
+    datos = ('Pago de saldo', saldo_actual, monto_pagado, nueva_deuda, id_cliente, tipo)
+    cursor.execute(sql, datos)
+    conn.commit()
 
-            # Actualiza el saldo del cliente en la base de datos
-            cursor.execute("UPDATE clientes_proveedores SET saldo=%s WHERE id=%s", (nueva_deuda, id))
-            conn.commit()
+    # Actualiza el saldo del cliente en la base de datos
+    cursor.execute("UPDATE clientes_proveedores SET saldo=%s WHERE id=%s", (nueva_deuda, id))
+    conn.commit()
 
-            flash("Pago procesado exitosamente.")
-        except Exception as e:
-            print(f"Error al procesar pago: {str(e)}")
-            flash("Error al procesar el pago. Por favor, inténtalo de nuevo.")
+        #     flash("Pago procesado exitosamente.")
+        # except Exception as e:
+        #     print(f"Error al procesar pago: {str(e)}")
+        #     flash("Error al procesar el pago. Por favor, inténtalo de nuevo.")
 
-        finally:
-            conn.close()
+        # finally:
+    conn.close()
 
         # Redirige a la página principal de clientes después de procesar el pago
+    if tipo_cliente == 1:
         return redirect("/clientes")
+    else:
+        return redirect("/proveedores")
 
-    # Si la solicitud no es un POST, redirige a la página principal
-    return redirect("/clientes")
+#Info Clientes
+@app.route('/ver_info_cliente/<int:id>', methods=['GET'])
+def ver_info_cliente(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    # conseguir info del cliente a partir del ID
+    cursor.execute("SELECT * FROM clientes_proveedores WHERE id=%s", (id,))
+    cliente = cursor.fetchone()
+
+    # conseguir info de los movimientos del cliente a partir del ID
+    cursor.execute("SELECT id, total, pago, diferencia_cliente, tipo  FROM detalles_compra WHERE id_cliente=%s", (id,))
+    movimiento = cursor.fetchall()
+
+    movimiento_list = list(movimiento)
+
+    movimiento_list2 = []
+    for i in movimiento_list:
+        movimiento_list2.append(list(i))
+
+    movimiento = movimiento_list2
+
+    for i in movimiento:
+        formateo = f'{i[0]:07}'
+        i[0] = formateo
+
+    # conseguir info de las compras del cliente a partir del ID
+    cursor.execute("SELECT productos FROM detalles_compra WHERE id_cliente=%s", (id,))
+    compras_json_rows = cursor.fetchall()
+    compras = []
+
+    for row in compras_json_rows:
+        compras_json = row[0]
+        if compras_json == 'Pago de saldo':
+            compras.append(compras_json)
+        else:
+            compras.append(json.loads(compras_json))
+
+    movimientos =  zip(movimiento, compras)
+
+    lista_temp = []
+    for i in movimientos:
+        lista_temp.insert(0, i)
+    
+    movimientos = lista_temp
+
+    # conseguir la suma del total
+    cursor.execute("SELECT SUM(total) FROM detalles_compra WHERE id_cliente=%s", (id,))
+    total = cursor.fetchone()[0]
+
+    # conseguir la suma del total
+    cursor.execute("SELECT SUM(pago) FROM detalles_compra WHERE id_cliente=%s", (id,))
+    pago = cursor.fetchone()[0]
+
+    # conseguir la suma del total
+    cursor.execute("SELECT SUM(diferencia_cliente) FROM detalles_compra WHERE id_cliente=%s", (id,))
+    diferencia_cliente = cursor.fetchone()[0]
+
+    conn.commit()
+    conn.close()
+
+    return render_template("clientes/info_cliente.html", cliente=cliente, movimientos=movimientos, total=total, pago=pago, diferencia_cliente=diferencia_cliente)
+
+#Info Proveedores
+@app.route('/ver_info_proveedores/<int:id>', methods=['GET'])
+def ver_info_proveedores(id):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    # conseguir info del proveedor a partir del ID
+    cursor.execute("SELECT * FROM clientes_proveedores WHERE id=%s", (id,))
+    proveedor = cursor.fetchone()
+
+    # conseguir info de los movimientos del proveedor a partir del ID
+    cursor.execute("SELECT id, total, pago, diferencia_cliente, tipo  FROM detalles_compra WHERE id_cliente=%s", (id,))
+    movimiento = cursor.fetchall()
+
+    movimiento_list = list(movimiento)
+
+    movimiento_list2 = []
+    for i in movimiento_list:
+        movimiento_list2.append(list(i))
+
+    movimiento = movimiento_list2
+    
+    for i in movimiento:
+        formateo = f'{i[0]:07}'
+        i[0] = formateo
+
+    # conseguir info de las compras al proveedor a partir del ID
+    cursor.execute("SELECT productos FROM detalles_compra WHERE id_cliente=%s", (id,))
+    compras_json_rows = cursor.fetchall()
+    compras = []
+
+    for row in compras_json_rows:
+        compras_json = row[0]
+        if compras_json == 'Pago de saldo':
+            compras.append(compras_json)
+        else:
+            compras.append(json.loads(compras_json))
+
+    movimientos =  zip(movimiento, compras)
+
+    lista_temp = []
+    for i in movimientos:
+        lista_temp.insert(0, i)
+    
+    movimientos = lista_temp
+
+    # conseguir la suma del total
+    cursor.execute("SELECT SUM(total) FROM detalles_compra WHERE id_cliente=%s", (id,))
+    total = cursor.fetchone()[0]
+
+    # conseguir la suma del total
+    cursor.execute("SELECT SUM(pago) FROM detalles_compra WHERE id_cliente=%s", (id,))
+    pago = cursor.fetchone()[0]
+
+    # conseguir la suma del total
+    cursor.execute("SELECT SUM(diferencia_cliente) FROM detalles_compra WHERE id_cliente=%s", (id,))
+    diferencia_cliente = cursor.fetchone()[0]
+
+    conn.commit()
+    conn.close()
+
+    return render_template("proveedores/info_proveedor.html", proveedor=proveedor, movimientos=movimientos, total=total, pago=pago, diferencia_cliente=diferencia_cliente)
 
 # Iniciar la aplicacion Flask
 if __name__ == "__main__":
